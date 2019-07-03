b05f8e9959a9b3fa414cf253be383df5d1895a55
Cody W. Eilar <Cody.Eilar@Gmail.com>
2019-07-02 18:19:47 -0600

    Start working on compliance test
    Signed-off-by: Cody W. Eilar <Cody.Eilar@Gmail.com>

--- __tests__/main.spec.ts
+++ __tests__/main.spec.ts
@@ -1,16 +1,15 @@
-// import LevelUp from 'levelup'
-var levelup = require('levelup')
+import LevelUp from 'levelup'
 import GaiaLevelDOWN from '../src/main';
 import {SessionInterface, GetFileOptions, PutFileOptions} from '../src/blockstack-interfaces'; 
-
+// const tapeTest = require('tape')
+// const suite = require('abstract-leveldown/test')
 
 class MockSession implements SessionInterface {
-
   store : any
   constructor() {
     this.store = {}; 
   }
-  
+
   async getFile(path: string, options?: GetFileOptions): Promise<string | ArrayBuffer> {
     return this.store[path];
   }
@@ -21,25 +20,40 @@ class MockSession implements SessionInterface {
 
 }
 
-describe('Test GaiaLevelDOWN capability', () => {
+describe('Test GaiaLevelDOWN get/put', () => {
   test('Put and get', () => {
     let mockSession = new MockSession(); 
     let gaiaDb = new GaiaLevelDOWN("location", mockSession); 
     console.log('gaiaDb = ', gaiaDb)
-    let db = levelup(gaiaDb)
+    let db = LevelUp(gaiaDb)
     db.put("key", "value")
-    // db.put("key", "Somevalue", {encrypt: true}, (err?) => {
-    //   if(err) throw new Error('Could not put key into the gaia storage!')
-    //   else {
-    //     db.get("key", (err, value) => {
-    //       if (err) throw new Error('Could not get the key!')
-    //       else {
-    //         expect(value).toBe("Somevalue")
-    //       }
-    //     })
-    //   }
-    // })
+    db.put("key", "Somevalue", {encrypt: true}, (err?) => {
+      if(err) throw new Error('Could not put key into the gaia storage!')
+      else {
+        db.get("key", (err, value) => {
+          if (err) throw new Error('Could not get the key!')
+          else {
+            expect(value).toBe("Somevalue")
+          }
+        })
+      }
+    })
     expect(true).toBe(true) 
 
   })
 });
+
+// describe('Prove GaiaLevelDOWN is abstract-leveldown compliant', () =>{
+//   test('Run compliance check',  () => {
+
+//     suite({
+//       test: tapeTest,
+//       factory: function () {
+//         let mockSession = new MockSession(); 
+//         let gaiaDb = new GaiaLevelDOWN("location", mockSession); 
+//         return gaiaDb
+//       }
+//     })
+//     expect(true).toBe(true)
+//   })
+// })
